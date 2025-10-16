beece0e0479f3b7f615daf844803f0bea8d7c1c1
Izyn <g00473082@atu.ie>
2025-10-16 11:41:31 +0100

    LAB4 PART_1

--- src/main/java/ie/atu/cloudnative/catalogservice/CatalogServiceApplication.java
+++ src/main/java/ie/atu/cloudnative/catalogservice/CatalogServiceApplication.java
@@ -2,6 +2,8 @@ package ie.atu.cloudnative.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
